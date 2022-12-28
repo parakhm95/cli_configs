ecbbe9febdf3b476d684fb6898821042c7d9b600
parakh <parakhm95@gmail.com>
2022-12-28 01:15:32 +0100

    Adding auto launch and startup for i3wm

--- ubuntu/shell_pmg.sh
+++ ubuntu/shell_pmg.sh
@@ -19,7 +19,7 @@ xset r rate 240 70
 alias home_on="xrandr --output eDP-1-1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --mode 2560x1440 --rate 164.80 --pos 1920x0 --rotate normal --set \"Broadcast RGB\" \"Full\" --output DP-1-1-1 --off --output DP-1-1-2 --off --output DP-1-1-3 --off"
 alias home_off="xrandr --output DP-1-3 --off"
 alias second-brain="cd ~/git/second-brain"
-export PS1="[\e[1;32m\u\e[0m|\e[1;34m\W\e[0m]\e[1;31m\$ \e[0m"
+# export PS1="[\e[1;32m\u\e[0m|\e[1;34m\W\e[0m]\e[1;31m\$ \e[0m"
 
 eval `keychain --eval parakh`
 setxkbmap -option caps:swapescape
