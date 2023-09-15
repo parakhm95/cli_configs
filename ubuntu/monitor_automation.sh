a7cfa765d52b97a3ce3251a973d5b03c33493367
Parakh M. Gupta <parakhm95@gmail.com>
2023-09-15 14:52:15 +0200

    Adding monitor config for office

--- ubuntu/monitor_automation.sh
+++ ubuntu/monitor_automation.sh
@@ -5,15 +5,19 @@ echo "Display is $DISPLAY"
 xrandr_output=$(xrandr -display :0 --prop)
 active_monitors=$(xrandr -display :0 --listactivemonitors)
 home_edid="1c540d2701010101"
+office_edid1="10acd9d054413732"
+office_edid2="10acd9d054303232"
 home_config_on(){
 xrandr xrandr -display :0.0 --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --dpi 100 --set "TearFree" "on" --output DisplayPort-1 --mode 2560x1440 --dpi 110 --rate 164.80 --pos 1920x0 --rotate normal --set "TearFree" "on"
 }
 home_on="xrandr --output eDP --primary --mode 1920x1080 --pos 0x0 --rotate normal --dpi 100 --set "TearFree" "on" --output DisplayPort-1 --mode 2560x1440 --dpi 110 --rate 164.80 --pos 1920x0 --rotate normal --set "TearFree" "on""
-home_off="xrandr --output DisplayPort-1 --off"
+home_off="xrandr --output DisplayPort-1 --off --output eDP --primary --dpi 100 --set "TearFree" "on""
+office_off="xrandr --output DisplayPort-3 --off --output DisplayPort-4 --off --output eDP --primary --dpi 100 --set "TearFree" "on""
+office_on="xrandr --output eDP --mode 1920x1080 --primary --pos 3000x183 --rotate normal --dpi 100 --output DisplayPort-3 --mode 1920x1080 --dpi 100 --pos 1080x243 --rotate normal --dpi 100 --set "TearFree" "on" --output DisplayPort-4 --mode 1920x1080 --pos 0x0 --rotate left --set "TearFree" "on""
+
 home_config_off(){
 xrandr --output DisplayPort-1 --off
 }
-office_edid=""
 # echo "$xrandr_output"
 if [[ $xrandr_output =~ $home_edid ]]
 then
@@ -32,6 +36,27 @@ else
         echo "Home Monitor active, switching off"
         $home_off
     else
-        echo "Home Monitor not active, doing nothing"
+        echo "Home Monitor inactive, doing nothing"
+    fi
+fi
+
+if [[ $xrandr_output =~ $office_edid1 || $xrandr_output =~ $office_edid2 ]]
+then
+    echo "Office Monitors are connected"
+    if [[ $active_monitors =~ "DisplayPort-3" || $active_monitors =~ "DisplayPort-4" ]]
+    then
+        echo "Office monitors already active, doing nothing"
+    else
+        echo "Office Monitors not active, activating"
+        $office_on
+    fi
+else
+    echo "Office Monitors are not connected"
+    if [[ $active_monitors =~ "DisplayPort" ]]
+    then
+        echo "Office monitors active, switching off"
+        $office_off
+    else
+        echo "Office monitors inactive, doing nothing"
     fi
 fi
