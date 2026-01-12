a33730400371df0198e02069fbb1eeda763beda5
T14-6-PMG <parakhm95@gmail.com>
2026-01-12 15:34:21 +0100

    Adding polybar config

--- ubuntu/polybar/launch.sh
+++ ubuntu/polybar/launch.sh
@@ -8,7 +8,20 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar topbar 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    if [ "$m" = "eDP" ]; then
+        MONITOR=$m polybar --reload topbar &
+        MONITOR=$m polybar --reload bottombar &
+    else
+        MONITOR=$m polybar --reload topbar-lowdpi &
+        MONITOR=$m polybar --reload bottombar-lowdpi &
+    fi
+  done
+else
+  polybar --reload topbar &
+fi
+# polybar topbar 2>&1 | tee -a /tmp/polybar1.log & disown
 # polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
 
 echo "Bars launched..."
